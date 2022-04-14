d195fe849edcd8f953046d98ef45bc54d08a2935
nanayaw-williams <youngwillies27@gmail.com>
2022-04-13 21:22:37 -0700

    not too sure

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,11 @@
  */
 void print_alphabet(void)
 {
-	int n;
+  char letter;
 
-	for (n = 'a'; n <= 'z'; n++)
-	{
-		_putchar(n);
-	}
-	_putchar('\n');
-        return (0);
+  for (letter = 'a'; letter <= 'z'; letter++)
+    _putchar(letter);
+
+  _putchar('\n');
+        
 }   
