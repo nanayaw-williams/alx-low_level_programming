7d2f34960c6f58a211dd2664e82d2e1ee090120f
nanayaw-williams <youngwillies27@gmail.com>
2022-04-13 21:10:38 -0700

    it maybe wrong

---
+++ 0x02-functions_nested_loops/0.putchat.c
@@ -0,0 +1,17 @@
+#include "main.h"                                                                                             
+                                                                                                              
+/**                                                                                                           
+ * main - check the code                                                                                      
+ * @void: no argument                                                                                         
+ * return 0;                                                                                                  
+**/                                                                                                           
+int main (void)                                                                                               
+{                                                                                                             
+char ermias[] = "_putchar"                                                                                    
+int c;                                                                                                        
+for (c = 0; c < 8; c++)                                                                                       
+{                                                                                                             
+_putchar(ermias[c]);
+}
+_putchar('\n');
+return (0);
