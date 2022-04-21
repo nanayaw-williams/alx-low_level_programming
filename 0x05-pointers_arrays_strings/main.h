01c3a2ed9b23235023c255f01a7eeaf899d02760
nana yaw williams <34478331+nanayaw-williams@users.noreply.github.com>
2022-04-21 04:12:46 +0000

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,16 +1,11 @@
-/* MAIN_H */
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int n);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
