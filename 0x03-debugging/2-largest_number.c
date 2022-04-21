9af804947e4c4fb1cbaac9968ca2413e81c7679f
nana yaw williams <34478331+nanayaw-williams@users.noreply.github.com>
2022-04-21 15:42:01 +0000

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -10,21 +11,24 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a >= b && a >= c)
-	{
-	largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-	largest = b;
-	}
-	else
-	{
-	largest = c;
-	}
-
-	return (largest);
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > a && b > c)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
 }
 
+return (largest);
+}
